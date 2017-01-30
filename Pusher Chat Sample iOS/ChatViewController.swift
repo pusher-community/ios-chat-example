7542f93d96433fbd44fa7b62ffdd40c4766aa749
Zan Markan <zan.markan@gmail.com>
2017-01-30 16:31:08 +0000

    Tableview that shows items and scrolls to the bottom

--- Pusher Chat Sample iOS/ChatViewController.swift
+++ Pusher Chat Sample iOS/ChatViewController.swift
@@ -8,8 +8,9 @@
 
 import Foundation
 import UIKit
+import PusherSwift
 
-class ChatViewController: UIViewController {
+class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet var message: UITextField!
     @IBAction func send(_ sender: Any) {
@@ -19,12 +20,66 @@ class ChatViewController: UIViewController {
         }
         
     }
+    @IBOutlet var messageTable: UITableView!
+    
+    let CellIdentifier = "com.pusher.TableViewCell"
     
     var twitterHandle: String = "Anonymous"
     
+    var pusher: Pusher?
+    
+    var array: NSMutableArray = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        self.messageTable.dataSource = self
+        messageTable.register(UITableViewCell.self, forCellReuseIdentifier: CellIdentifier)
+        
+
+        let options = PusherClientOptions(
+            host: .cluster("eu")
+        )
+        pusher = Pusher(
+            key: "f400c100bb3d7301c79a",
+            options: options
+        )
+        
+        // subscribe to channel and bind to event
+        let channel = pusher!.subscribe("my-channel")
+        
+        let _ = channel.bind(eventName: "my-event", callback: { (data: Any?) -> Void in
+            if let data = data as? [String : AnyObject] {
+                if let message = data["message"] as? String {
+                    self.array.add(message)
+    
+                    DispatchQueue.main.async{
+                        self.messageTable.reloadData()
+                        let indexPath = IndexPath(item: self.array.count-1, section: 0)
+                        self.messageTable.scrollToRow(at: indexPath, at: UITableViewScrollPosition.none, animated: true)
+
+                    }
+                }
+            }
+        })
+        
+        pusher!.connect()
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier, for: indexPath) as UITableViewCell
+//        cell.textLabel?.text = data[indexPath.row]
+        cell.textLabel?.text = array.object(at: indexPath.row) as! String
+        return cell
+    }
+    
+    
     
     func twitterHandle(twitterHandle: String){
         self.twitterHandle = twitterHandle
